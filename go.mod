638d106b9b88d5d6e6e9c8e1597297e36e899abe
Józef Daniecki <jozek.daniecki@gmail.com>
2024-10-31 18:17:36 +0100

    Simplifications.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jdaniecki/url-shortener
 
 go 1.23.2
+
+require github.com/magefile/mage v1.15.0
--- magefile.go
+++ magefile.go
@@ -5,48 +5,36 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
+
+	"github.com/magefile/mage/sh"
 )
 
 // Lint runs markdownlint on all Markdown files
 func Lint() error {
 	fmt.Println("Running markdownlint...")
-	cmd := exec.Command("markdownlint", "**/*.md")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	return sh.RunV("markdownlint", "**/*.md")
 }
 
 // Test runs the tests
 func Test() error {
 	fmt.Println("Running tests...")
-	cmd := exec.Command("go", "test", "./...")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	return sh.RunV("go", "test", "./...")
 }
 
 // Build builds the binary
 func Build() error {
 	fmt.Println("Building binary...")
-	cmd := exec.Command("go", "build", "-o", "build/url-shortener", "./cmd/url-shortener")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	return sh.RunV("go", "build", "-o", "build/url-shortener", "./cmd/url-shortener")
 }
 
 // Run runs the binary
 func Run() error {
 	fmt.Println("Running binary...")
-	cmd := exec.Command("./build/url-shortener")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	return sh.RunV("./build/url-shortener")
 }
 
 // Clean removes the build directory
 func Clean() error {
 	fmt.Println("Removing build directory...")
-	return os.RemoveAll("build")
+	return sh.Rm("build")
 }
