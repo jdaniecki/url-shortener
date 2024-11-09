151954643e5d0105a05fe3a4749c9b2602bb60a8
Józef Daniecki <jozek.daniecki@gmail.com>
2024-11-09 13:17:41 +0100

    Fix linter.

--- internal/server/server.go
+++ internal/server/server.go
@@ -37,7 +37,9 @@ func (s *Server) PostShorten(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	resp := map[string]string{"shortUrl": "http://short.url/" + shortUrl}
-	json.NewEncoder(w).Encode(resp)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
 }
 
 func (s *Server) GetShortUrl(w http.ResponseWriter, r *http.Request, shortUrl string) {
@@ -47,5 +49,7 @@ func (s *Server) GetShortUrl(w http.ResponseWriter, r *http.Request, shortUrl st
 		return
 	}
 	resp := map[string]string{"originalUrl": originalUrl}
-	json.NewEncoder(w).Encode(resp)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
 }
